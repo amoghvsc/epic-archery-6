e783aeeb3891cbf1503bd81a475dfabc02f6adba
amoghvsc <108701829+amoghvsc@users.noreply.github.com>
2023-02-02 19:48:53 -0500

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -55,7 +55,7 @@ class PlayerArrow {
     image(this.image, 0, 0, this.width, this.height);
     pop();
 
-    if (this.body.velocity.x > 0 && this.body.position.x > 400) {
+    if (this.body.velocity.x > 0 && this.body.position.x > 500) {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
